719296ff7761e306175ff5d95a1b6722b4e52110
conconcon <conductcellox3@gmail.com>
2025-05-06 03:25:37 +0900

    CSS Modulesの練習

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from './About.module.css';
+
+export default function About(){
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>CSS練習ページ</h1>
+        </div>
+    )
+}
